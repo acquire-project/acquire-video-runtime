f098408eb16ef2551e9a7d399d9bfa134587be2d
Nathan Clack <nclack@chanzuckerberg.com>
2023-04-18 08:46:49 -0700

    write-side-by-side-tiff passes

---
+++ tests/write-side-by-side-tiff.cpp
@@ -0,0 +1,155 @@
+#include "acquire.h"
+#include "device/hal/device.manager.h"
+#include "platform.h"
+#include "logger.h"
+
+#include <cstdio>
+#include <stdexcept>
+#include <cstring>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+void
+reporter(int is_error,
+         const char* file,
+         int line,
+         const char* function,
+         const char* msg)
+{
+    fprintf(is_error ? stderr : stdout,
+            "%s%s(%d) - %s: %s\n",
+            is_error ? "ERROR " : "",
+            file,
+            line,
+            function,
+            msg);
+}
+
+/// Helper for passing size static strings as function args.
+/// For a function: `f(char*,size_t)` use `f(SIZED("hello"))`.
+/// Expands to `f("hello",5)`.
+#define SIZED(str) str, sizeof(str) - 1
+
+#define L (aq_logger)
+#define LOG(...) L(0, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define ERR(...) L(1, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define EXPECT(e, ...)                                                         \
+    do {                                                                       \
+        if (!(e)) {                                                            \
+            char buf[1 << 8] = { 0 };                                          \
+            ERR(__VA_ARGS__);                                                  \
+            snprintf(buf, sizeof(buf) - 1, __VA_ARGS__);                       \
+            throw std::runtime_error(buf);                                     \
+        }                                                                      \
+    } while (0)
+#define CHECK(e) EXPECT(e, "Expression evaluated as false: %s", #e)
+#define DEVOK(e) CHECK(Device_Ok == (e))
+#define OK(e) CHECK(AcquireStatus_Ok == (e))
+
+void
+acquire(AcquireRuntime* runtime, const char* filename)
+{
+    auto dm = acquire_device_manager(runtime);
+    CHECK(runtime);
+    CHECK(dm);
+
+    AcquireProperties props = {};
+    OK(acquire_get_configuration(runtime, &props));
+
+    DEVOK(device_manager_select(dm,
+                                DeviceKind_Camera,
+                                SIZED("simulated.*random.*"),
+                                &props.video[0].camera.identifier));
+    DEVOK(device_manager_select(dm,
+                                DeviceKind_Storage,
+                                SIZED("tiff-json"),
+                                &props.video[0].storage.identifier));
+
+    const char external_metadata[] = "{\"hello\":\"world\"}";
+
+    props.video[0].storage.settings.external_metadata_json = {
+        .str = (char*)external_metadata,
+        .nbytes = sizeof(external_metadata),
+        .is_ref = 1,
+    };
+
+    props.video[0].storage.settings.filename = {
+        .str = (char*)filename,
+        .nbytes = strlen(filename) + 1,
+        .is_ref = 1,
+    };
+
+    props.video[0].camera.settings.binning = 1;
+    props.video[0].camera.settings.pixel_type = SampleType_u8;
+    props.video[0].camera.settings.shape = { .x = 64, .y = 48 };
+    // we may drop frames with lower exposure
+    props.video[0].camera.settings.exposure_time_us = 1e4;
+    props.video[0].max_frame_count = 70;
+
+    OK(acquire_configure(runtime, &props));
+
+    const auto next = [](VideoFrame* cur) -> VideoFrame* {
+        return (VideoFrame*)(((uint8_t*)cur) + cur->bytes_of_frame);
+    };
+
+    const auto consumed_bytes = [](const VideoFrame* const cur,
+                                   const VideoFrame* const end) -> size_t {
+        return (uint8_t*)end - (uint8_t*)cur;
+    };
+
+    struct clock clock;
+    static double time_limit_ms = 20000.0;
+    clock_init(&clock);
+    clock_shift_ms(&clock, time_limit_ms);
+    OK(acquire_start(runtime));
+    {
+        uint64_t nframes = 0;
+        while (nframes < props.video[0].max_frame_count) {
+            struct clock throttle;
+            clock_init(&throttle);
+            EXPECT(clock_cmp_now(&clock) < 0,
+                   "Timeout at %f ms",
+                   clock_toc_ms(&clock) + time_limit_ms);
+            VideoFrame *beg, *end, *cur;
+            OK(acquire_map_read(runtime, 0, &beg, &end));
+            for (cur = beg; cur < end; cur = next(cur)) {
+                LOG("stream %d counting frame w id %d", 0, cur->frame_id);
+                CHECK(cur->shape.dims.width ==
+                      props.video[0].camera.settings.shape.x);
+                CHECK(cur->shape.dims.height ==
+                      props.video[0].camera.settings.shape.y);
+                ++nframes;
+            }
+            {
+                uint32_t n = consumed_bytes(beg, end);
+                OK(acquire_unmap_read(runtime, 0, n));
+                if (n)
+                    LOG("stream %d consumed bytes %d", 0, n);
+            }
+            clock_sleep_ms(&throttle, 100.0f);
+
+            LOG(
+              "stream %d nframes %d time %f", 0, nframes, clock_toc_ms(&clock));
+        }
+
+        CHECK(nframes == props.video[0].max_frame_count);
+    }
+
+    OK(acquire_stop(runtime));
+}
+
+int
+main()
+{
+
+    auto runtime = acquire_init(reporter);
+    const char* filename = TEST ".dir";
+    acquire(runtime, filename);
+    CHECK(fs::is_directory(filename));
+    CHECK(fs::exists(fs::path(filename) / "metadata.json"));
+    CHECK(fs::exists(fs::path(filename) / "data.tif"));
+    LOG("Done (OK)");
+    acquire_shutdown(runtime);
+    return 0;
+}
